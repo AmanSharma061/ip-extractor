c33cf1127fed6c848a3756178d4ef59eacc86219
Aman <b121061@iiit-bh.ac.in>
2024-03-27 21:37:07 +0530

    final

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 
-
+// Define a route to get the IP address
 app.get('/ip', (req, res) => {
- 
-  const ipAddress = req.ip.replace('::ffff:', ''); 
+  // Extract the IP address from the request headers
+  let ipAddress = req.ip.replace('::ffff:', ''); // Remove IPv6 prefix
+  if (ipAddress === '::1') {
+    // If IP address is localhost, replace with 127.0.0.1
+    ipAddress = '127.0.0.1';
+  }
   
+  // Send the IP address as the response
   res.send({ip:ipAddress});
 });
 
